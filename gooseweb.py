9808939d93aecec0693fab6a0c1f5f14a962f056
kj4ohh <kj4ohh@gmail.com>
2012-08-06 22:44:48 -0400

    moved some things around, added content to some of the pages

--- gooseweb.py
+++ gooseweb.py
@@ -5,7 +5,6 @@ from flask import Flask, request, session, g, redirect, \
      url_for, abort, render_template, flash
 
 # configuration
-#DATABASE = '/tmp/flaskr.db'
 DEBUG = True
 SECRET_KEY = 'development key'
 
@@ -16,32 +15,6 @@ app.config.from_object(__name__)
 app.config.from_envvar('GOOSEWEB_SETTINGS', silent=True)
 
 
-#def connect_db():
-#    """Returns a new connection to the database."""
-#    return sqlite3.connect(app.config['DATABASE'])
-
-
-#def init_db():
-#    """Creates the database tables."""
-#    with closing(connect_db()) as db:
-#        with app.open_resource('schema.sql') as f:
-#            db.cursor().executescript(f.read())
-#        db.commit()
-
-
-#@app.before_request
-#def before_request():
-#    """Make sure we are connected to the database each request."""
-#    g.db = connect_db()
-
-
-#@app.teardown_request
-#def teardown_request(exception):
-#    """Closes the database again at the end of the request."""
-#    if hasattr(g, 'db'):
-#        g.db.close()
-
-
 @app.route('/')
 def root():
     return redirect(url_for('home'))
@@ -50,22 +23,24 @@ def root():
 def home():
     return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.route('/community')
 def community():
-    return 'You clicked community...'
+    return render_template('community.html')
 
 @app.route('/download')
 def download():
-    return 'You clicked download...'
+    return render_template('download.html')
 
 @app.route('/help')
 def help():
-    return 'You clicked help...'
+    return render_template('help.html')
 
-@app.route('/about')
-def about():
-    return 'You clicked about...'
-
-# If we were not loaded as a module, start the app instance
+#############################################################
+# If we were not loaded as a module, start the app instance #
+#############################################################
 if __name__ == '__main__':
     app.run()
