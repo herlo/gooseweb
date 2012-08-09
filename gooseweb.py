6cb72e5168fa97dd19ecbeb8aebd74db4f2c94ef
kj4ohh <kj4ohh@gmail.com>
2012-08-09 07:16:31 -0400

    added rss news feed capability to home page

--- gooseweb.py
+++ gooseweb.py
@@ -9,6 +9,7 @@ from contextlib import closing
 from flask import Flask, request, session, g, redirect, \
      url_for, abort, render_template, flash
 import feedparser
+import os
 
 
 #----------------------------------------------------------------
@@ -16,6 +17,7 @@ import feedparser
 #----------------------------------------------------------------
 DEBUG = True
 SECRET_KEY = 'development key'
+POSTSFILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/postsfile.html')
 
 
 #----------------------------------------------------------------
@@ -35,7 +37,28 @@ def root():
 
 @app.route('/home')
 def home():
+
+    # grab the feed from planet gooseproject
+    feed = feedparser.parse('http://planet.gooseproject.org/atom.xml')
+    # open temp postsfile for writing
+    postsfile = open(POSTSFILE, 'w')
+    
+    # format each post for placement on the home page
+    for entry in feed.entries:
+        postsfile.write('<span class="post_author">')
+        postsfile.write(unicode(entry.author).encode('utf8'))
+        postsfile.write('</span>')
+        postsfile.write('<h3>')
+        postsfile.write(unicode(entry.title).encode('utf8'))
+        postsfile.write('</h3>')
+        postsfile.write(entry.updated)  #or update-parsed
+        postsfile.write(unicode(entry.content[0].value).encode('utf8'))
+        postsfile.write(entry.link)
+
+    postsfile.close()
+
     return render_template('home.html')
+
 
 @app.route('/about')
 def about():
